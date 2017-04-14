cfb0d8741d018035dc28ffb6a4729d331cfe8f1e
rdsequie <rdsequie@uwaterloo.ca>
2017-04-14 16:22:46 -0400

    testing integration

---
+++ idf_baseline/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='animemon',
+      description='End2end question answering pipeline',
+      packages=['animemon'],
+      dependency_links=['https://github.com/castorini/Castor#egg=sm_model'],
+      )
