3e4ff3fffda2a34c7b0a0cba4cc8e6cb124bd9f6
Gaurav Baruah <gbaruah@uwaterloo.ca>
2017-06-08 15:29:59 -0400

    distributed dep parsing

--- sm_cnn_linguistic/dependency_parse.py
+++ sm_cnn_linguistic/dependency_parse.py
@@ -1,6 +1,8 @@
 from nltk.parse.stanford import StanfordDependencyParser
 
 import string
+import argparse
+import os
 
 path_to_jar = 'stanford-corenlp-full-2016-10-31/stanford-corenlp-3.7.0.jar'
 path_to_models_jar = 'stanford-corenlp-full-2016-10-31/stanford-corenlp-3.7.0-models.jar'
@@ -19,5 +21,28 @@ def get_dependency_reordered(sentence):
         reordered_tokens.append(item[2][0])
     return ' '.join(reordered_tokens)
 
+
+def write_out_deps(filename, data):
+    with open(filename, "w") as outf:
+        oldq = None
+        oldqdep = None
+        for q in data:
+            if oldq != q:
+                oldqdep = get_dependency_reordered(q)
+                oldq = q                 
+            print(oldqdep if len(oldqdep) else "a_placeholder_term", file=outf)
+
+
 if __name__ == "__main__":
-    print(dependency_parse("I like mangoes"))
+
+    ap = argparse.ArgumentParser(description='dependecy parseing and reordering of sentence tokens')
+    ap.add_argument("input_file")
+
+    ap.add_argument("offset", type=int)
+    ap.add_argument("batch_size", type=int)
+    args = ap.parse_args()
+
+    questions = [line.strip() for line in open(args.input_file).readlines()][args.offset:args.batch_size]
+    write_out_deps(args.input_file + ".deps.{:06d}".format(args.offset), questions)
+
+
