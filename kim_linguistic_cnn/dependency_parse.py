36e4800e81e21d2d23cd3357740275960f45fbd3
rosequ <rdsequie@uwaterloo.ca>
2017-08-28 19:21:04 -0400

    initial codebase

---
+++ kim_linguistic_cnn/dependency_parse.py
@@ -0,0 +1,74 @@
+import pickle
+
+from nltk.parse.stanford import StanfordDependencyParser
+
+import string
+import argparse
+import os
+import sys
+import re
+
+path_to_jar = 'stanford-corenlp-full-2016-10-31/stanford-corenlp-3.7.0.jar'
+path_to_models_jar = 'stanford-corenlp-full-2016-10-31/stanford-corenlp-3.7.0-models.jar'
+dependency_parser = StanfordDependencyParser(path_to_jar=path_to_jar, path_to_models_jar=path_to_models_jar)
+r = re.compile(r'\d{3,100}[ -]+\d{3,100}[ -]+\d{3,100}')
+
+def dependency_parse(sentence):
+    try:
+        result = dependency_parser.raw_parse(sentence)
+        dep = result.__next__()
+        return list(dep.triples())
+    except:
+        print("EXCEPTION: while parsing sentence:")
+        sys.exit(0)
+
+
+def get_dependency_reordered(sentence):
+    sentence = sentence.replace("/", "")
+    results = r.findall(sentence)
+
+    for res in results:
+        replaced = res.replace('-', "_")
+        replaced = res.replace(' ', "_")
+        print(replaced)
+        sentence = sentence.replace(res, replaced)
+
+    deps = dependency_parse(sentence)
+    return deps
+    # reordered_tokens = []
+    # for item in deps:
+    #     reordered_tokens.append(item[0][0])
+    #     reordered_tokens.append(item[2][0])
+    # return deps, ' '.join(reordered_tokens)
+
+
+# def write_out_pickle(filename, data):
+#     with open("{}.all.pickle".format(filename), "w") as outf:
+#         oldq = None
+#         oldqdep = None
+#         for q in data:
+#             if oldq != q:
+#                 oldqdep = get_dependency_reordered(q)
+#                 oldq = q
+#             pickle.dump(oldqdep if len(oldqdep) else "a_placeholder_term", file=outf)
+
+
+def write_out_deps(input, output):
+    with open(input, 'r') as inputf, open(output, 'w') as outf:
+        for line in inputf:
+            det = line.strip().split(' ', 1)
+            outf.write(str(dependency_parse(det[1])) + "\n")
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser(description='dependnecy parsing and reordering of sentence tokens')
+    ap.add_argument("input")
+    args = ap.parse_args()
+
+    write_out_deps(args.input, args.input + '.deps')
+    # questions = ["I killed an elephant in my pajamas"]
+    # # questions = [line.strip() for line in open(args.input_file).readlines()][args.offset:args.offset+args.batch_size]
+    # # write_out_pickle(args.input_file + ".deps.{:06d}".format(args.offset), questions)
+    # write_out_pickle( "a.deps.{:06d}".format(10), questions)
+    # x = pickle.load(open("a.deps.000010.all.pickle", "rb"))
+    # print(x)
+    #
